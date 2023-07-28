f5e9c73be67d485d537f6ab944c407ce1e59192f
jeffnuyens <jeffreynuyens@ufm.edu>
2023-07-28 08:12:27 -0600

    R Summary 26 jul

---
+++ RepasoR/RepasoR 26 jul.R
@@ -0,0 +1,36 @@
+install.packages("dplyr")
+install.packages("RMySQL")
+install.packages("lubridate")
+install.packages("openxlsx")
+install.packages("tidyverse")
+install.packages("stringr")
+install.packages("readr")
+install.packages("ggplor2")
+
+library(dplyr) #cargar la libreria que deseo 
+
+
+
+getwd() #mostrar en en qué carpeta estoy trabajando 
+setwd()
+
+#char
+string <- "This is a string"
+string
+#logical
+logical <- FALSE
+logical 
+#int
+integer <- 2L
+integer 
+#double
+number <- 234
+number
+#vector
+vec1 <- c(1,2,3, "A")
+#ordered vector 
+#data set 
+#factores
+#lists
+#indexing 
+
